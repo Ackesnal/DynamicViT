402d2f427a1415c351ee0dbb19cffe36fca255ac
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2022-04-14 16:24:42 +1000

    Update losses.py

--- losses.py
+++ losses.py
@@ -219,7 +219,7 @@ class DistillDiffPruningLoss(torch.nn.Module):
                                                      log_target=True)
         
         # print(cls_loss, pred_loss)
-        loss = self.clf_weight * cls_loss + self.distill_weight * cls_kl_loss + self.cut_weight * cut_loss / len(self.pruning_loc) + self.distill_weight * 100 * token_kl_loss 
+        loss = self.clf_weight * cls_loss + self.cut_weight * cut_loss / len(self.pruning_loc) + self.distill_weight * 100 * token_kl_loss # + self.distill_weight * cls_kl_loss  
 
         if self.print_mode:
             self.cls_loss += cls_loss.item()
