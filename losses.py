e03c8a9eb52817519222b29fed34af6c50ebc516
Ackesnal <36218184+Ackesnal@users.noreply.github.com>
2022-05-06 12:12:50 +1000

    Update losses.py

--- losses.py
+++ losses.py
@@ -119,6 +119,7 @@ class DiffPruningLoss(torch.nn.Module):
             cut =  intra_loss + inter_loss # B
             
             cut_loss = cut_loss + cut
+            del out_attns[i], out_attn_masks[i]
         
         # classification loss
         cls_loss = self.base_criterion(pred, labels)
